1f7200fc7f3165fdfdb32667fd0fa6b0c6435cf9
Nikita Nesterov <nik.nest1@gmail.com>
2020-05-01 14:27:11 +0200

    test for LayoutGalerie was added(tests copy function with a random data)

---
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/LayoutGalerieTest.java
@@ -0,0 +1,74 @@
+package org.jis.generator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LayoutGalerieTest {
+	
+	private LayoutGalerie galerieUnderTest;
+	private File fromFile;
+	private File toFile;
+
+	/**
+	 * Before-Test method for {@link org.jis.generator.LayoutGalerie#copyFile(File, File)}.
+	 */
+	@Before
+	public final void setUp() {
+		galerieUnderTest = new LayoutGalerie(null, null);
+	}
+
+	/**
+	 * After-Test method for {@link org.jis.generator.LayoutGalerie#copyFile(File, File)}.
+	 */
+	@After
+	public final void tearDown() {
+		galerieUnderTest = null;
+	}
+
+	/**
+	 * Test method for {@link org.jis.generator.LayoutGalerie#copyFile(File, File)}.
+	 */
+	@Test
+	public final void testCopyFile() throws URISyntaxException {
+		try {
+			final File resourceFolder = new File(this.getClass().getResource(File.separator).toURI());
+			fromFile = new File(resourceFolder, "from");
+			toFile = new File(resourceFolder, "to");
+			
+			byte[] array = new byte[10];
+			new Random().nextBytes(array);
+			String randomString = new String(array);
+		 			 
+			fromFile.createNewFile();
+			Path fromPath = FileSystems.getDefault().getPath(fromFile.getPath());
+			Files.writeString(fromPath, randomString);
+			 
+			galerieUnderTest.copyFile(fromFile, toFile);
+			 
+			assertTrue(toFile.exists());
+			 
+			Path toPath = FileSystems.getDefault().getPath(toFile.getPath());
+			String contents = Files.readString(toPath);
+			 		 
+			assertEquals(randomString, contents);
+		 }
+		 catch (IOException | URISyntaxException e) {
+			fail();
+		 }
+		
+	}
+
+}
