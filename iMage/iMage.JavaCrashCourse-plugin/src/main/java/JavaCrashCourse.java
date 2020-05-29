9c4b22433dd369bd69609a1b5c04aee14855c3a8
Nikita Nesterov <nik.nest1@gmail.com>
2020-05-29 15:47:04 +0200

    meta-inf service supportion added, bugs fixed

--- iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
+++ iMage/iMage.JavaCrashCourse-plugin/src/main/java/JavaCrashCourse.java
@@ -1,16 +1,23 @@
 import org.iMage.plugins.PluginForJmjrst;
 import org.jis.Main;
+import org.kohsuke.MetaInfServices;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
+@MetaInfServices(PluginForJmjrst.class)
 public class JavaCrashCourse extends PluginForJmjrst {
     private List<String> javaVersionsList;
-    Main main;
-    JFrame frame;
+    private String choosenJavaVersion = "";
+    private Main main;
+    private JFrame frameStart;
+    private JFrame frameConfigure;
     final private String[] javaVersions = {
             "Java SE 8(LTS)",
             "Java SE 9",
@@ -44,6 +51,35 @@ public class JavaCrashCourse extends PluginForJmjrst {
         System.out.println(String.format("Found %d Java versions since Java 8", getNumberOfParameters()));
         this.main = main;
 
+        JFrame frameStart = new JFrame("Java Crash Course");
+        frameStart.setLocationRelativeTo(null);
+        frameStart.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frameStart.setPreferredSize(new Dimension(250, 150));
+        JPanel panelStart = new JPanel();
+        panelStart.setLayout(new BoxLayout(panelStart, BoxLayout.PAGE_AXIS));
+        JLabel msg;
+        switch(choosenJavaVersion) {
+            case "Java SE 8(LTS)":
+                msg = new JLabel("Keeping updated");
+                break;
+            case "Java SE 9",
+                    "Java SE 10",
+                    "Java SE 11(LTS)",
+                    "Java SE 12",
+                    "Java SE 13",
+                    "Java SE 14",
+                    "Java SE 15",
+                    "Java SE 16",
+                    "Java SE 17(LTS)":
+                msg = new JLabel("Running late");
+                break;
+            default:
+                msg = new JLabel("JavaCrashCourse(7)");
+                break;
+        }
+        panelStart.add(msg);
+        frameStart.pack();
+        frameStart.setVisible(true);
     }
 
     @Override
@@ -58,17 +94,27 @@ public class JavaCrashCourse extends PluginForJmjrst {
 
     @Override
     public void configure() {
-        frame = new JFrame("frame name");
-        frame.setLocationRelativeTo(main);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        // TODO: fill the frame
+        frameConfigure = new JFrame("Java Versions");
+        frameConfigure.setPreferredSize(new Dimension(250, 260));
+        frameConfigure.setLayout(new BorderLayout());
+        frameConfigure.setLocationRelativeTo(main);
+        frameConfigure.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        ButtonGroup radioButtons = new ButtonGroup();
         javaVersionsList.stream().forEach((version) -> {
-            JLabel jVersion = new JLabel(version);
-            main.add(jVersion);
+            JRadioButton jVersion = new JRadioButton(version);
+            jVersion.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    choosenJavaVersion = e.getActionCommand();
+                }
+            });
+            radioButtons.add(jVersion);
+            panel.add(jVersion);
         });
-        //
-        main.pack();
-        frame.setVisible(true);
-
+        frameConfigure.add(panel);
+        frameConfigure.pack();
+        frameConfigure.setVisible(true);
     }
 }
